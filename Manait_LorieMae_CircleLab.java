fade464560c5977601f9ac657fcedb807ebd12da
lmaetrix <manaitlorie@g.cjc.edu.ph>
2024-12-09 23:20:22 +0900

    Update Manait_LorieMae_CircleLab.java

--- Manait_LorieMae_CircleLab.java
+++ Manait_LorieMae_CircleLab.java
@@ -21,7 +21,7 @@ public class Manait_LorieMae_CircleLab {
                 String input = JOptionPane.showInputDialog("Enter the radius for circle:");
                 double userRadius = Double.parseDouble(input);
                 Circle customCircle = new Circle(userRadius);
-                JOptionPane.showMessageDialog(null, "\nCustom Circle:" + "\nArea: " + customCircle.area() + "\nCircumference: " + customCircle.circumference());
+                JOptionPane.showMessageDialog(null, "\nCustom Circle:" + "\nArea: " + customCircle.area() + "\nCircumference: " + customCircle.circumference() + "\n Diameter: " + customCircle.diameter());
                 int respo = JOptionPane.showConfirmDialog(null, "Do you want to perform another calculation?", "Continue?", JOptionPane.YES_NO_OPTION);
                 if (respo == JOptionPane.NO_OPTION) {
                     loop = false;
@@ -29,9 +29,9 @@ public class Manait_LorieMae_CircleLab {
             } else if (opt == JOptionPane.NO_OPTION) {
                 Circle defaultCircle = new Circle();
                 JOptionPane.showMessageDialog(null, "Default Circle: with an 8.5 value" + "\n Area: " + defaultCircle.area() + "\n Circumference: " + defaultCircle.circumference());
-                int[] radii = {5, 4, 6, 7, 2, 4};
+                int[] radii = {1, 5};
                 for (int i = 0; i < radii.length; i++) {
-                    JOptionPane.showMessageDialog(null, "Default Circle: with a radius of " + radii[i] + "\n Area: " + defaultCircle.arearadii(radii[i]) + "\n Circumference: " + defaultCircle.circumradii(radii[i]));
+                    JOptionPane.showMessageDialog(null, "Default Circle: with a radius of " + radii[i] + "\n Area: " + defaultCircle.arearadii(radii[i]) + "\n Circumference: " + defaultCircle.circumradii(radii[i]) + "\n Diameter: " + defaultCircle.diameterradii(radii[i]));
                 }
                 int res = JOptionPane.showConfirmDialog(null, "Would you like to perform another calculation?", "Continue?", JOptionPane.YES_NO_OPTION);
                 if (res == JOptionPane.NO_OPTION) {
